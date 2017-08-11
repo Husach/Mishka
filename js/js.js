54d06307362f9d6fa6594ab679f708f8fbcf78e9
Andrii Husach <andrii.husach@gmail.com>
2017-08-11 15:39:44 +0300

    'js_in_menu-btn'

---
+++ js/js.js
@@ -0,0 +1,18 @@
+var menu = document.querySelector(".main-nav");
+var list = document.querySelector(".main-nav__list");
+var btn = document.querySelector(".main-nav__toggle");
+var open = document.querySelector(".main-nav--opened");
+
+
+btn.addEventListener("click", function(event) {
+	event.preventDefault();
+	if (list.classList.contains("main-nav__list--closed")) {
+		list.classList.remove("main-nav__list--closed");
+		menu.classList.toggle("main-nav--closed");
+		menu.classList.toggle("main-nav--opened");
+	} else {
+		list.classList.add("main-nav__list--closed");
+		menu.classList.toggle("main-nav--closed");
+		menu.classList.toggle("main-nav--opened");
+	}
+});
