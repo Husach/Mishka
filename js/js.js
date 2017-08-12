b6c08b12b056dd13c6e765fb639b11faad7aea75
Andrii Husach <andrii.husach@gmail.com>
2017-08-12 13:59:04 +0300

    'tablet_version'

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var menu = document.querySelector(".main-nav");
 var list = document.querySelector(".main-nav__list");
 var btn = document.querySelector(".main-nav__toggle");
@@ -8,11 +10,11 @@ btn.addEventListener("click", function(event) {
 	event.preventDefault();
 	if (list.classList.contains("main-nav__list--closed")) {
 		list.classList.remove("main-nav__list--closed");
-		menu.classList.toggle("main-nav--closed");
-		menu.classList.toggle("main-nav--opened");
+		menu.classList.remove("main-nav--closed");
+		menu.classList.add("main-nav--opened");
 	} else {
 		list.classList.add("main-nav__list--closed");
-		menu.classList.toggle("main-nav--closed");
-		menu.classList.toggle("main-nav--opened");
+		menu.classList.add("main-nav--closed");
+		menu.classList.remove("main-nav--opened");
 	}
 });
